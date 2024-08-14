139b9694a26007fc13dc15cb8f9bd395a86431aa
Yezer <109930585+YezerSTN@users.noreply.github.com>
2024-08-13 20:59:47 -0600

    remove

--- js/pp-critical.js
+++
@@ -1,8 +0,0 @@
-;(() => {
-  let theme = localStorage.getItem('theme')
-  if (!theme) {
-    const dark = window.matchMedia('(prefers-color-scheme: dark)').matches
-    theme = dark ? 'night' : 'day'
-  }
-  document.documentElement.classList.add(`theme-${theme}`)
-})()
